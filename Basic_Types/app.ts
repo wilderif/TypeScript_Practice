f3017c56898bc32d7f99d6fadb8e89c8577d7813
wilderif <woojuro3@gmail.com>
2024-08-06 02:35:05 +0900

    feat(Basic_Types): Learn how to specify parameter types in functions and type inference
    - Studied how to specify types for parameters in functions
    - Learned about TypeScript's type inference feature

---
+++ Basic_Types/app.ts
@@ -0,0 +1,20 @@
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase, result);
+  } else {
+    return result;
+  }
+}
+
+// const number1 = 5;
+let number1: number;
+number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+// const printResult = false;
+// const resultPhrase = "Result is: ";
+let resultPhrase = "Result is: ";
+
+const result = add(number1, number2, printResult, resultPhrase);
+console.log(result);
