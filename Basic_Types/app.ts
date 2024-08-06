9af5cf7f32b58c3d768f3eb1cd5b9ec465a1ab78
wilderif <woojuro3@gmail.com>
2024-08-06 12:43:43 +0900

    feat(Basic_Types): Learn return types, void type, and function types with arrow functions
    - Studied return types and void type in functions
    - Learned how to specify parameter and return types in arrow functions for Function types
    - Practiced using Function types to define callback functions

--- Basic_Types/app.ts
+++ Basic_Types/app.ts
@@ -1,38 +1,23 @@
-type Combinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
-
-function combine(
-  input1: number | string,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result: number | string;
-
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  return result;
-
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
 
-const combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+printResult(add(5, 12));
 
-console.log("test run");
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+console.log(combineValues(5, 15));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
