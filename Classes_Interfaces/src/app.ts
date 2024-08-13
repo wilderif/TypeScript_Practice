ee4d0fdb1a3badbc57a4f47998d73e1a001c2b3a
wilderif <woojuro3@gmail.com>
2024-08-13 17:38:00 +0900

    feat(Classes_Interfaces): Learn about ‘this’ keyword, access modifiers, and readonly properties
    - Learned the usage of the ‘this’ keyword within class methods
    - Learned about the private, public keywords and access modifiers
    - Learned readonly properties in classes

--- Classes_Interfaces/src/app.ts
+++ Classes_Interfaces/src/app.ts
@@ -1,11 +1,37 @@
 class Department {
-  name: string;
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
 
-  constructor(name: string) {
-    this.name = name;
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = name;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department("d1", "Accounting");
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+// accounting.employees[2] = "Anna";
 
 console.log(accounting);
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { describe: accounting.describe, name: "DUMMY" };
+// accountingCopy.describe();
