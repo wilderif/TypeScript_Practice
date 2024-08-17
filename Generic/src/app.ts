f53203e7ef0730e1a71cc8b83a18e493414be9c3
wilderif <woojuro3@gmail.com>
2024-08-17 21:39:07 +0900

    feat(Generic): Learn to use interfaces with generics and the ‘keyof’ keyword
    - Explored how to use interfaces to set compatibility with generic types
    - Studied the ‘keyof’ keyword and its application

--- Generic/src/app.ts
+++ Generic/src/app.ts
@@ -25,3 +25,30 @@ const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergedObj);
 console.log(mergedObj.name);
 console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
+console.log(countAndDescribe(["Sports", "Cooking"]));
+console.log(countAndDescribe([]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+console.log(extractAndConvert({ name: "Max", age: 30 }, "name"));
