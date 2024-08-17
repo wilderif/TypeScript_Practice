81ec3bdc3239698adc7a53144623b812ae6aa226
wilderif <woojuro3@gmail.com>
2024-08-17 20:13:41 +0900

    feat(Advanced_Types): Learn about Index Signatures and Function Overloads
    - Studied Index Signatures and how to use them to define flexible interfaces
    - Learned how to implement Function Overloads to define different return types based on input types

--- Advanced_Types/src/app.ts
+++ Advanced_Types/src/app.ts
@@ -21,12 +21,19 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
   }
   return a + b;
 }
+
+const result = add("Max", " Schwarz");
+result.split(" ");
 
 type UnknownEmployee = Employee | Admin;
 
@@ -112,3 +119,12 @@ const userInputElement = document.getElementById("user-input");
 if (userInputElement) {
   (userInputElement as HTMLInputElement).value = "Hi there!";
 }
+
+interface ErrorContainer {
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: "Not a valid email!",
+  username: "Must start with a capital character!",
+};
