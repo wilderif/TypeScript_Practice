468a73253b8281515bd77d46883fe8c02128de76
wilderif <woojuro3@gmail.com>
2024-08-21 14:47:44 +0900

    feat: Add CSS id to created element in constructor and enhance form element access
    - Added functionality in the constructor to assign a CSS id to the element being created.
    - Selected 'title', 'description', and 'people' elements inside the form for access.
    - Defined 'configure' function to set up form submission event listener.
    - Implemented 'submitHandler' function as a callback for form submission to handle form data.

--- Drag_and_Drop/src/app.ts
+++ Drag_and_Drop/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -14,7 +17,31 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", (event) => {
+      this.submitHandler(event);
+    });
   }
 
   private attach() {
@@ -23,4 +50,3 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
-ㅇ;
