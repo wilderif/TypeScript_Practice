4960ce21a70ae3f15f68f7e0097fd93901275e63
wilderif <woojuro3@gmail.com>
2024-08-21 15:26:53 +0900

    feat(Drag_and_Drop): Implement autobind and enhance form handling
    - Inplement and applied 'autobind' decorator to ensure 'this' in 'submitHandler' correctly references the class instance.
    - Implemented 'gatherUserInput' to validate form input and return a tuple with the gathered values.
    - Updated 'submitHandler' to utilize 'gatherUserInput' for form data processing.

--- Drag_and_Drop/src/app.ts
+++ Drag_and_Drop/src/app.ts
@@ -1,3 +1,17 @@
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -33,15 +47,35 @@ class ProjectInput {
     this.attach();
   }
 
+  private getherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid input, please try again!");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.getherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+    }
   }
 
   private configure() {
-    this.element.addEventListener("submit", (event) => {
-      this.submitHandler(event);
-    });
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
